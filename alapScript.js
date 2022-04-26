e842fbf100c2ffb9f67ead0b1273585c2424e3c4
dokibacsi <kvalcsi02@gmail.com>
2022-04-26 15:58:53 +0200

    kiindulas_alap

---
+++ alapScript.js
@@ -0,0 +1,11 @@
+function ID(elem){
+    return document.getElementById(elem);
+}
+
+function CLS(elem){
+    return document.getElementsByClassName(elem);
+}
+
+function $(elem){
+    return document.querySelectorAll(elem);
+}
