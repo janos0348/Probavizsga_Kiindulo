3eee627a874d8e5f3e844646c5a5311938cf9a50
janos0348 <katonajanos0128@gmail.com>
2022-05-10 14:48:16 +0200

    kiirva

--- galeria.js
+++ galeria.js
@@ -1,5 +1,28 @@
-fetch('galeria.json')
+window.addEventListener("load", init)
+
+function ID(elem){
+    return document.getElementById(elem);
+}
+
+function $(elem){
+    return document.querySelectorAll(elem);
+}
+
+function init(){
+  var tervezok = "";
+  fetch('galeria.json')
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      console.log(data.zerowaste)
+      proba(data.zerowaste,tervezok);
     });
+    document.getElementById('alapGaleria').innerHTML = tervezok;
+}
+function proba(tomb,tervezok){
+  for (let i = 0; i < tomb.length; i++) {
+    console.log(tomb[i].tervezo)
+    tervezok+='Tervező: ' + tomb[i].tervezo+"<br>";
+    
+  }
+  document.getElementById('alapGaleria').innerHTML = tervezok;
+}
